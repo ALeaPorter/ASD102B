e76c479c6c3ddc46df7001eb598baacc1c85830a
amberporter <260000988@ciat.edu>
2026-09-19 12:59:10 -0600

    Initial commit Countdown

---
+++ Countdown/server.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("."));
+
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
